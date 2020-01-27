effb31ffe26c74d311eb8d1eea8c68820d82079d
Noel Glamann <nglamann@debian08.CIS.COM>
2020-01-27 08:15:45 -0600

    new file:   creating_menu_options.py

---
+++ creating_menu_options.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+#Noel Glamann
+#22 January 2020
+
+''' this is going to be used to develop my own program 
+following in the footsteps of our example with big_brother.py.'''
+
+#---IMPORT-------------------------------------------
+
+
+
+#---FUNCTIONS----------------------------------------
+
+
+
+#---MAIN PROGRAM-------------------------------------
+
+print('''
+          MOVIE DATABASE
+            MAIN MENU
+        ------------------
+        
+1. Add Game
+2. Edit Existing Game
+3.
+4.
+5.
+6.
+
+Q. Quit
+
+#TODO
+
